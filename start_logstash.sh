8e2f472ba41941cbda5773f9f83a0554c7e9bf10
jack <dikien2012@gmail.com>
2016-11-07 15:58:38 +0900

    fixed

--- start_logstash.sh
+++ start_logstash.sh
@@ -5,7 +5,6 @@ tar -xvzf ~/Downloads/logstash-2.3.0.tar.gz
 cp  ~/Downloads/osquery-osx-conf/logstash-output-sumologic-1.0.3.gem  ~/Downloads/osquery-osx-conf/logstash-2.3.0/bin/
 sudo mv ~/Downloads/osquery-osx-conf/logstash-2.3.0 /usr/local/logstash/
 /usr/local/logstash/bin/logstash-plugin install /usr/local/logstash/bin/logstash-output-sumologic-1.0.3.gem
-openssl enc -d -aes-256-cbc -a -in ~/Downloads/osquery-osx-conf/logstash.conf.enc -out ~/Downloads/osquery-osx-conf/logstash.conf
 sudo cp ~/Downloads/osquery-osx-conf/logstash.conf /usr/local/logstash/logstash.conf
 sudo cp ~/Downloads/osquery-osx-conf/com.elastic.logstash.plist /Library/LaunchDaemons/
 sudo launchctl load /Library/LaunchDaemons/com.elastic.logstash.plist
